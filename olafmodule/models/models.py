6e9d2b8c845d5c320ac0920bec765f27fdbb0f10
ozo7 <osc@odoo.com>
2020-01-28 13:41:23 +0000

    add requirements.txt

--- olafmodule/models/models.py
+++ olafmodule/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from unidecode import unidecode
 from odoo import models, fields, api
 
 
@@ -12,6 +13,19 @@ class olafmodule(models.Model):
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+    
+    @api.model
+    def create(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+        return super(my_module, self).create(values)
+
+    @api.multi
+    def write(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+        return super(my_module, self).write(values)
+    
     @api.depends('value')
     def _value_pc(self):
         for record in self:
