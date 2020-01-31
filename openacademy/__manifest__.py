9a95d71575c44c1e04bd47bfb9fa3f9b7d8a984a
ozo7 <send2olaf@yahoo.de>
2020-01-31 11:19:19 +0100

    working openacademy b4 inheritance

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,9 +25,10 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        #'views/views.xml',
         'views/templates.xml',
-        #'views/openacademy.xml',
+        'views/openacademy.xml',
+        'demo/demo.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -2,12 +2,12 @@
 
 from odoo import models, fields, api
 
-class openacademy(models.Model):
-    _name = 'openacademy.openacademy'
-    _description = 'openacademy.openacademy'
-    
-    name = fields.Char()
-    description = fields.Text()
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+#     _description = 'openacademy.openacademy'
+#
+#     name = fields.Char()
+#     description = fields.Text()
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -15,3 +15,21 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    # one2many means that each session is uniquely linked to a course
+    # TODO: I should try to make it many2many since session could just be a timeframe relating to many courses like the summer vacations
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
