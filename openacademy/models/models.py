cf96dad4de3539af2cb1d6096bfaca16e3ca4a79
ozo7 <send2olaf@yahoo.de>
2020-02-03 16:59:24 +0100

    gantt with other configuration than in the tutorial

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 
@@ -56,14 +57,25 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     # domain declared as list is server-side evaluated
     # domain declared as a string is client-side evaluated
-    instructor_id = fields.Many2one('res.partner', string="Instructor", \
-                                    domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
+    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
     # the above is relevant for the selection of an instructor from the form view of the session instance
     # OK -- why does it not offer the Teachers to be selected? => server restart resolved this problem
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+    hours = fields.Float(string="Duration in hours", compute='_get_hours', inverse='_set_hours')
+    testingField = fields.Integer(default=7)
+
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours // 24
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -72,6 +84,27 @@ class Session(models.Model):
                 r.taken_seats = 0.0
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            r.duration = (r.end_date - r.start_date).days + 1
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
