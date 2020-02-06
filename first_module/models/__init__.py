a1a2ca73e8f413ddeed9bf36ee5280688d59ea4b
ozo7 <send2olaf@yahoo.de>
2020-02-06 16:38:20 +0100

    cleaning up after resolving the gender combo relation/calculation

--- first_module/models/__init__.py
+++ first_module/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import mot_de_langue
 
--- first_module/models/models.py
+++ first_module/models/models.py
@@ -1,18 +1,73 @@
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class first_module(models.Model):
-#     _name = 'first_module.first_module'
-#     _description = 'first_module.first_module'
+class MotType(models.Model):
+    _name = 'vv.mot.type'
+    _rec_name = 'mottype' # specifies a column to be shown for the option to link to other records in the frontend
+    _description = 'basic grammar type of word'
+    _log_access=False
+    mottype = fields.Char('type of word')
+    explanation = fields.Char('explanation')
+    zz_mot_de_langue_ids = fields.One2many(
+        'vv.mot.de.langue', 'mottype_id',
+        string='words of this grammar type',
+    )
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class MotDeLangue(models.Model):
+    _name = 'vv.mot.de.langue'
+    _description = 'a single vocabulary item'
+    fr = fields.Char('French')
+    de = fields.Char('German')
+    en = fields.Char('English')
+    mottype_id = fields.Many2one(
+         'vv.mot.type', string='grammar type of word',
+         ondelete='set null',
+    )
+    # ==> we do not want the standard many2many because we want to expand this table by custom columns.
+    # learners_ids = fields.Many2many(
+    #     'res.partner', string='learners',
+    # )
+
+    category = fields.Char('none')
+    tags = fields.Char('no tags')
+
+class Noun(models.Model):
+    _name = 'vv.noun'
+    _description = 'noun that as a word specialty can have a gender'
+    _inherit = 'vv.mot.de.langue'
+
+    gender_combo = fields.Many2one('vv.gender.combo', string='gender sequence for the different languages')
+
+class GenderCombo(models.Model):
+    _name = 'vv.gender.combo'
+    _rec_name = 'combo'  
+    _description = 'gender combinations table as reference for nouns'
+    _log_access=False
+
+    fr = fields.Char('French')
+    de = fields.Char('German')
+    combo = fields.Char('combo')
+    noun_ids = fields.One2many('vv.noun', 'gender_combo', string='vocabulary word')
+
+
+
+# try to create an explicit model to extend the many2many table information
+class WordsXLearners(models.Model):
+    _name = 'vv.mots.learners'
+    _description = 'Words N Learners Relations'
+    _log_access=False   # does it really prevent the log audit fields => yes, it does!
+
+    mot_de_langue_id = fields.Many2one(
+        'vv.mot.de.langue',
+        string='word ID',
+        required=True,
+    )
+
+    res_partner_id = fields.Many2one(
+        'res.partner',
+        string='learner ID',
+        required=True,
+    )
+
+    timesLearnt = fields.Integer(default=0)
+
